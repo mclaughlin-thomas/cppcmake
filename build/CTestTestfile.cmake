8618e10c8b495ef3fd7968f9ac8d2a7d07c48717
mclaughlin-thomas <90363044+mclaughlin-thomas@users.noreply.github.com>
2024-05-14 20:36:41 -0400

    init commit CMAKE

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Handrail/Desktop/Program Languages/cppcmake
+# Build directory: C:/Users/Handrail/Desktop/Program Languages/cppcmake/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>  // This includes the input-output stream library
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
